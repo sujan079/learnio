712f91726f187a06a0347ca1ceacca0cb4b1110a
sujan079 <sujantimalsina079@gmail.com>
2020-05-27 19:48:12 +0545

    "Spalsh Screen"

--- app/src/main/java/com/example/learnio/SplashScreenActivity.java
+++ app/src/main/java/com/example/learnio/SplashScreenActivity.java
@@ -1,14 +1,28 @@
 package com.example.learnio;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private Integer delay_time = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        start();
+    }
+
+    public void start() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent activity = new Intent(SplashScreenActivity.this, WelcomeActivity.class);
+            }
+        }, delay_time);
     }
 }
