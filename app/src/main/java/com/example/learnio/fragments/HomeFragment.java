18feb58dcc34281579880eddba9e3f820512dc88
sujan079 <sujantimalsina079@gmail.com>
2020-05-29 20:09:23 +0545

    Sign Sing up

--- app/src/main/java/com/example/learnio/fragments/HomeFragment.java
+++ app/src/main/java/com/example/learnio/fragments/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +12,11 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.learnio.R;
 import com.example.learnio.adapter.CategoryAdapter;
 import com.example.learnio.model.Category;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +25,9 @@ public class HomeFragment extends Fragment {
     private RecyclerView rvCategory;
     private CategoryAdapter categoryAdapter;
     private RecyclerView.LayoutManager categoryLayoutManger;
+    private ImageView ivProfile;
+
+    FirebaseAuth firebaseAuth;
 
     public HomeFragment() {
     }
@@ -37,8 +43,11 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
         initCategory();
+        setIvProfile();
     }
+
 
     public void initCategory() {
         rvCategory = getView().findViewById(R.id.rv_category);
@@ -50,5 +59,14 @@ public class HomeFragment extends Fragment {
         rvCategory.setAdapter(categoryAdapter);
         rvCategory.setLayoutManager(categoryLayoutManger);
         rvCategory.setHasFixedSize(true);
+    }
+
+    public void setIvProfile() {
+        ivProfile = getView().findViewById(R.id.iv_profile);
+        Glide
+                .with(ivProfile)
+                .load(firebaseAuth.getCurrentUser().getPhotoUrl())
+                .placeholder(R.drawable.dummy)
+                .into(ivProfile);
     }
 }
--- app/src/main/java/com/example/learnio/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/learnio/fragments/ProfileFragment.java
@@ -1,19 +1,30 @@
 package com.example.learnio.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 import com.example.learnio.R;
+import com.example.learnio.SplashScreenActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+
+    FirebaseAuth firebaseAuth;
+    private Button signOutButton;
+    private ImageView ivProfile;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -26,4 +37,38 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+        init();
+
+    }
+
+    public void init() {
+        signOutButton = getView().findViewById(R.id.btn_logout);
+        ivProfile = getView().findViewById(R.id.iv_profile);
+
+        setIvProfile();
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                Intent intent = new Intent(getActivity(), SplashScreenActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void setIvProfile() {
+        Glide
+                .with(ivProfile)
+                .load(firebaseAuth.getCurrentUser().getPhotoUrl())
+                .placeholder(R.drawable.dummy)
+                .into(ivProfile);
+    }
+
+
 }
