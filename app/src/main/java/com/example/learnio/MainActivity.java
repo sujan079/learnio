067cafe924d624d4da0ee862fb7d513ee9ebb3fb
sujan079 <sujantimalsina079@gmail.com>
2020-05-29 18:56:12 +0545

    Sign Sing up

--- app/src/main/java/com/example/learnio/MainActivity.java
+++ app/src/main/java/com/example/learnio/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.learnio;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.learnio.adapter.CategoryAdapter;
+import com.example.learnio.model.Category;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvCategory;
+    private CategoryAdapter categoryAdapter;
+    private RecyclerView.LayoutManager categoryLayoutManger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initCategory();
+    }
+
+    public void initCategory() {
+        rvCategory = findViewById(R.id.rv_category);
+        categoryAdapter = new CategoryAdapter();
+        categoryLayoutManger = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        categoryAdapter.setCategories(Category.getCategories());
+
+        rvCategory.setAdapter(categoryAdapter);
+        rvCategory.setLayoutManager(categoryLayoutManger);
+        rvCategory.setHasFixedSize(true);
     }
 }
--- app/src/main/java/com/example/learnio/adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/learnio/adapter/CategoryAdapter.java
@@ -26,7 +26,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     @NonNull
     @Override
     public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(parent, R.layout.category_item, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_item, parent, false);
 
         return new CategoryViewHolder(view);
     }
