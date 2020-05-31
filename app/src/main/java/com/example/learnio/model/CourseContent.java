38426b506031dcb8b66d9282af90c84cfea5c495
sujan079 <sujantimalsina079@gmail.com>
2020-05-31 15:36:59 +0545

    Sign Sing up

---
+++ app/src/main/java/com/example/learnio/model/CourseContent.java
@@ -0,0 +1,28 @@
+package com.example.learnio.model;
+
+public class CourseContent {
+    private String title;
+    private String duration;
+
+    public CourseContent(String title, String duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
+
--- app/src/main/java/com/example/learnio/model/Courses.java
+++ app/src/main/java/com/example/learnio/model/Courses.java
@@ -8,6 +8,12 @@ public class Courses {
     private String courseName;
     private String length;
     private int background;
+    private int users;
+    private double rating;
+
+    private ArrayList<CourseContent> courseContents;
+
+
 
     public Courses(String courseName, String length) {
         this.courseName = courseName;
@@ -19,6 +25,39 @@ public class Courses {
         this.courseName = courseName;
         this.length = length;
         this.background = background;
+    }
+
+    public Courses(String courseName, String length, int background, int users, double rating, ArrayList<CourseContent> courseContents) {
+        this.courseName = courseName;
+        this.length = length;
+        this.background = background;
+        this.users = users;
+        this.rating = rating;
+        this.courseContents = courseContents;
+    }
+
+    public int getUsers() {
+        return users;
+    }
+
+    public void setUsers(int users) {
+        this.users = users;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public ArrayList<CourseContent> getCourseContents() {
+        return courseContents;
+    }
+
+    public void setCourseContents(ArrayList<CourseContent> courseContents) {
+        this.courseContents = courseContents;
     }
 
     public String getCourseName() {
@@ -47,7 +86,10 @@ public class Courses {
 
     public static ArrayList<Courses> getCourses() {
         return new ArrayList<Courses>() {{
-            add(new Courses("Introduction to Computer Science", "4 hrs", R.drawable.blue));
+            add(new Courses("Introduction to Computer Science",
+                    "4 hrs",
+                    R.drawable.blue
+            ));
             add(new Courses("Introduction to Graphic Design", "2 hrs", R.drawable.pink));
             add(new Courses("Game Development", "5 hr", R.drawable.green));
             add(new Courses("Hello", "6 hrs", R.drawable.black));
